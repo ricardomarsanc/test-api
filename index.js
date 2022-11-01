036ce072e14a54b1d7a39f3fca9215865b526add
Ricardo Martín <ricardomartin@playtomic.io>
2022-11-01 12:32:48 +0100

    refactor: removed unused code

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const Tracing = require('@sentry/tracing')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-// const Note = require('./models/Note')
 
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
 
 const notFound = require('./middleware/notFound.js')
 const handleErrors = require('./middleware/handleError.js')
-// const User = require('./models/User')
 
 app.use(cors())
 app.use(express.json())
@@ -37,92 +35,12 @@ app.use(Sentry.Handlers.requestHandler())
 // TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler())
 
-// app.use(logger)
-
 app.get('/', (request, response) => {
   console.log(request.ip)
   console.log(request.ips)
   console.log(request.orignalUrl)
   response.send('<h1>Hello World</h1>')
 })
-
-// app.get('/api/notes', async (request, response) => {
-//   const notes = await Note.find({}).populate('user', {
-//     username: 1
-//   })
-//   response.json(notes)
-// })
-
-// app.get('/api/notes/:id', (request, response, next) => {
-//   const { id } = request.params
-
-//   Note.findById(id).then(note => {
-//     if (note) { response.json(note) }
-//     response.status(404).end()
-//   }).catch(err => next(err))
-// })
-
-// app.put('/api/notes/:id', (request, response, next) => {
-//   const { id } = request.params
-//   const note = request.body
-
-//   const newNoteInfo = {
-//     content: note.content,
-//     important: note.important
-//   }
-
-//   Note.findByIdAndUpdate(id, newNoteInfo, { new: true })
-//     .then(note => {
-//       response.json(note)
-//     })
-//     .catch(next)
-// })
-
-// app.delete('/api/notes/:id', async (request, response, next) => {
-//   const { id } = request.params
-
-//   try {
-//     const res = await Note.findByIdAndDelete(id)
-//     if (res === null) return response.sendStatus(404)
-//     response.status(204).end()
-//   } catch (e) {
-//     next(e)
-//   }
-// })
-
-// app.post('/api/notes', async (request, response, next) => {
-//   const {
-//     content,
-//     important = false,
-//     userId
-//   } = request.body
-
-//   const user = await User.findById(userId)
-
-//   if (!content) {
-//     return response.status(400).json({
-//       error: 'required "content" field is missing'
-//     })
-//   }
-
-//   const newNote = new Note({
-//     content,
-//     date: new Date().toISOString(),
-//     important,
-//     user: user._id
-//   })
-
-//   try {
-//     const savedNote = await newNote.save()
-//     // Add the new note to the user note list
-//     user.notes = user.notes.concat(savedNote._id)
-//     await user.save()
-
-//     response.json(savedNote)
-//   } catch (e) {
-//     next(e)
-//   }
-// })
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
