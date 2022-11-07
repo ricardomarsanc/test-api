2a6a3f800383382f77ed20d6499490f4228e597a
Ricardo Martín <ricardomartin@playtomic.io>
2022-11-07 19:16:05 +0100

    feat: added testingRouter

--- index.js
+++ index.js
@@ -51,6 +51,11 @@ app.use('/api/notes', notesRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 // -----------------------------------------------------------------
 // MIDDLEWARES
 // -----------------------------------------------------------------
